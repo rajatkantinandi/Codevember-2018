7d97543a5d34237c17d05734a1be934d0e995acc
Rajat Kanti Nandi <rajatkantinandi1993@gmail.com>
2018-11-11 04:35:40 +0530

    added controls

--- Day10-Apple/index.js
+++ Day10-Apple/index.js
@@ -1,15 +1,16 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
 );
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+var controls = new THREE.OrbitControls(camera, renderer.domElement);
 var x = 0,
-  y = 0;
+    y = 0;
 
 var heartShape = new THREE.Shape();
 
@@ -22,31 +23,32 @@ heartShape.bezierCurveTo(x + 16, y + 7, x + 16, y, x + 10, y);
 heartShape.bezierCurveTo(x + 7, y, x + 5, y + 5, x + 5, y + 5);
 
 var extrudeSettings = {
-  depth: 3,
-  bevelEnabled: true,
-  bevelSegments: 2,
-  steps: 0.5,
-  bevelSize: 1,
-  bevelThickness: 1
+    depth: 3,
+    bevelEnabled: true,
+    bevelSegments: 2,
+    steps: 0.5,
+    bevelSize: 1,
+    bevelThickness: 1
 };
 
 var geometry = new THREE.ExtrudeGeometry(heartShape, extrudeSettings);
 
 var mesh = new THREE.Mesh(
-  geometry,
-  new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true })
+    geometry,
+    new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true })
 );
 scene.add(mesh);
 camera.position.z = 50;
 var update = function() {
-  mesh.rotation.y += 0.01;
+    mesh.rotation.y += 0.01;
 };
 var render = function() {
-  renderer.render(scene, camera);
+    renderer.render(scene, camera);
 };
 var draw = function() {
-  requestAnimationFrame(draw);
-  update();
-  render();
+    requestAnimationFrame(draw);
+    controls.update();
+    update();
+    render();
 };
 draw();
