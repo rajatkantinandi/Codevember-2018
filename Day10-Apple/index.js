87952edf4c1c84a24913dca7a184ad3b140991e7
Rajat Kanti Nandi <rajatkantinandi1993@gmail.comi>
2018-11-11 21:29:20 +0530

    Created apple

--- Day10-Apple/index.js
+++ Day10-Apple/index.js
@@ -1,54 +1,55 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
 );
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 var controls = new THREE.OrbitControls(camera, renderer.domElement);
-var x = 0,
-    y = 0;
-
-var heartShape = new THREE.Shape();
-
-heartShape.moveTo(x + 5, y + 5);
-heartShape.bezierCurveTo(x + 5, y + 5, x + 4, y, x, y);
-heartShape.bezierCurveTo(x - 6, y, x - 6, y + 7, x - 6, y + 7);
-heartShape.bezierCurveTo(x - 6, y + 11, x - 3, y + 15.4, x + 5, y + 19);
-heartShape.bezierCurveTo(x + 12, y + 15.4, x + 16, y + 11, x + 16, y + 7);
-heartShape.bezierCurveTo(x + 16, y + 7, x + 16, y, x + 10, y);
-heartShape.bezierCurveTo(x + 7, y, x + 5, y + 5, x + 5, y + 5);
-
-var extrudeSettings = {
-    depth: 3,
-    bevelEnabled: true,
-    bevelSegments: 2,
-    steps: 0.5,
-    bevelSize: 1,
-    bevelThickness: 1
+var params = {
+  radius: 30,
+  tube: 80,
+  radialSeg: 25,
+  tubularSeg: 20
 };
-
-var geometry = new THREE.ExtrudeGeometry(heartShape, extrudeSettings);
-
+var geometry = new THREE.TorusGeometry(
+  params.radius,
+  params.tube,
+  params.radialSeg,
+  params.tubularSeg
+);
+var ang = 0;
 var mesh = new THREE.Mesh(
-    geometry,
-    new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true })
+  geometry,
+  new THREE.MeshPhongMaterial({ color: 0xff0000 })
 );
 scene.add(mesh);
-camera.position.z = 50;
+// var PointLight = new THREE.PointLight(0xffffff, 0.5);
+// PointLight.position.set(50, 50, 250);
+// scene.add(PointLight);
+var PointLight2 = new THREE.PointLight(0xffffff, 0.6);
+PointLight2.position.set(-50, 150, 250);
+scene.add(PointLight2);
+var PointLight3 = new THREE.PointLight(0xffffff, 0.6);
+PointLight3.position.set(-50, -200, 150);
+scene.add(PointLight3);
+var Ambient3 = new THREE.AmbientLight(0xffffff, 0.1);
+scene.add(Ambient3);
+camera.position.z = 350;
+mesh.rotation.x += -0.8;
 var update = function() {
-    mesh.rotation.y += 0.01;
+  mesh.rotation.z += 0.1;
 };
 var render = function() {
-    renderer.render(scene, camera);
+  renderer.render(scene, camera);
 };
 var draw = function() {
-    requestAnimationFrame(draw);
-    controls.update();
-    update();
-    render();
+  requestAnimationFrame(draw);
+  controls.update();
+  update();
+  render();
 };
 draw();
